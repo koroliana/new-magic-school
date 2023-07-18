71c6d4d25099b9bd6da3ca0ebf681aefab355afc
Elena Koroleva <koroliana@yandex.ru>
2023-07-18 22:37:43 +0700

    only renamed .yml and .sql files for liquibase

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -1,6 +1,7 @@
 --liquibase formatted sql
 
 --changeset koroleva:create_student_name_index
+--precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM my_table
 CREATE INDEX students_name_index ON students (name);
 
 --changeset koroleva:create_faculties_name_color_index
