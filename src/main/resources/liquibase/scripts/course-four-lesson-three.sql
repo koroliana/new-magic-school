1dc1625b662148031f0d2951a9d344829a48ed2b
Elena Koroleva <koroliana@yandex.ru>
2023-07-20 14:28:12 +0700

    added precondition and rollback to liquibase formatted sql

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -1,8 +1,13 @@
 --liquibase formatted sql
 
 --changeset koroleva:create_student_name_index
---precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM my_table
+--precondition onFail:MARK_RAN
+--precondition-sql-check expectedResult:0 select count(*) from pg_catalog.pg_tables t inner join pg_indexes i on i.tablename = t.tablename where t.tablename = 'students' and i.indexname = 'students_name_index';
+--rollback DROP INDEX students_name_index
 CREATE INDEX students_name_index ON students (name);
 
 --changeset koroleva:create_faculties_name_color_index
+--precondition onFail:MARK_RAN
+--precondition-sql-check expectedResult:0 select count(*) from pg_catalog.pg_tables t inner join pg_indexes i on i.tablename = t.tablename where t.tablename = 'faculties' and i.indexname = 'faculties_name_color_index';
+--rollback DROP INDEX faculties_name_color_index
 CREATE INDEX faculties_name_color_index ON faculties (name, color);
